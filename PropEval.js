ea1e2c5bb52f538aac8c1a9610a42e9a065477c3
Sharif <16631353+MakingPi@users.noreply.github.com>
2018-03-01 22:14:06 -0500

    minor code cleanup, adding different types of expense estimates

--- PropEval.js
+++ PropEval.js
@@ -14,12 +14,21 @@ function getPayment( amount, apr, months ){
  return M;
 }
 
+function getSelectedRadioValue( id ){
+  var radio_group = document.getElementById( id );
+  for (i=0; i<radio_group.childElementCount; i++) {
+    if (radio_group.children[i].checked) {
+      return radio_group.children[i].value;
+    }
+  }
+  return null;
+}
+
 function valueUpdated() {
  // read variables
  var full_price = parseInt(document.getElementById("full_price").value);
  var taxes = parseInt(document.getElementById("taxes").value);
  var repair_cost = parseInt(document.getElementById("repair_cost").value);
- var cash_down = parseInt(document.getElementById("cash_down").value);
  var prosper_down = parseInt(document.getElementById("prosper_down").value);
  var k401_down = parseInt(document.getElementById("k401_down").value);
  var mtgt_apr = parseFloat(document.getElementById("mortgage_apr").value);
@@ -29,21 +38,29 @@ function valueUpdated() {
  var snow_payments = parseInt(document.getElementById("snow_payments").value);
 
  // read radio buttons
- var tax_term_radios = document.getElementById("tax_term_radios");
- var tax_months;
- for (i=0; i<tax_term_radios.childElementCount; i++) {
-   if (tax_term_radios.children[i].checked) {
-     tax_months = parseInt(tax_term_radios.children[i].value);
-   }
- }
+ var tax_months = parseInt(getSelectedRadioValue('tax_term_radios'));
+ var mtgt_term = parseInt(getSelectedRadioValue('mtgt_term_radios'));
+ var cash_down_radios_val = getSelectedRadioValue('cash_down_radios');
+ var mx_radios_val        = getSelectedRadioValue('mx_radios');
+ var vacancy_radios_val   = getSelectedRadioValue('vacancy_radios');
+ var mgmt_radios_val      = getSelectedRadioValue('mgmt_radios');
 
- var mtgt_term_radios = document.getElementById("mtgt_term_radios");
- var mtgt_term;
- for (i=0; i<mtgt_term_radios.childElementCount; i++) {
-   if (mtgt_term_radios.children[i].checked) {
-     mtgt_term = parseInt(mtgt_term_radios.children[i].value);
-   }
+ var cash_down = document.getElementById("cash_down");
+ switch( cash_down_radios_val )
+ {
+   case 'custom':
+    cash_down.disabled = false;
+    break;
+    case '20p':
+    cash_down.disabled = true;
+    cash_down.value = ((0.20 * full_price) + repair_cost - k401_down - prosper_down).toFixed(2);
+    break;
+    case '25p':
+    cash_down.disabled = true;
+    cash_down.value = ((0.25 * full_price) + repair_cost - k401_down - prosper_down).toFixed(2);
+    break;
  }
+ cash_down = parseInt(cash_down.value);
 
  // do fancy calculations
  var mtgt_amount = (full_price + repair_cost) - (cash_down + prosper_down + k401_down);
@@ -58,16 +75,53 @@ function valueUpdated() {
 
  // fill out simple calculations
  var loan_payments = long_payment + short_401k + short_p2p;
- var tax_payments = (taxes / tax_months);
- var maint_payments = (gross_rent * 0.15); // 15%
- var vacancy_payments = (gross_rent * 0.08); // 8%
- var mgmt_payments = (gross_rent * .1); // 10%
-
  document.getElementById("loan_payments").value = loan_payments.toFixed(2);
+ var tax_payments = (taxes / tax_months);
  document.getElementById("tax_payments").value = tax_payments.toFixed(2);
- document.getElementById("maint_payments").value = maint_payments.toFixed(2);
- document.getElementById("vacancy_payments").value = vacancy_payments.toFixed(2);
- document.getElementById("mgmt_payments").value = mgmt_payments.toFixed(2);
+
+ var maint_payments = document.getElementById("maint_payments");
+ switch( mx_radios_val ){
+    case '15pRent':
+      maint_payments.value = (gross_rent * 0.15).toFixed(2); // 15%
+      maint_payments.disabled = true;
+      break;
+    case '1pValue':
+      maint_payments.value = (((full_price + repair_cost) * 0.01) / 12).toFixed(2); // monthly rate for 1% annually
+      maint_payments.disabled = true;
+      break;
+    case 'custom':
+      maint_payments.disabled = false;
+      break;
+ }
+ maint_payments = parseInt(maint_payments.value);
+
+ var vacancy_payments = document.getElementById("vacancy_payments");
+ switch( vacancy_radios_val ){
+   case '1yr':
+      vacancy_payments.value = (gross_rent/11).toFixed(2); // need to get a full month rent from 11 months of living
+      break;
+   case '2yr':
+     vacancy_payments.value = (gross_rent/23).toFixed(2); // need to get a full month rent from 23 months of living
+     break;
+   case '5yr':
+     vacancy_payments.value = (gross_rent/59).toFixed(2); // need to get a full month rent from 59 months of living
+     break;
+ }
+ vacancy_payments = parseInt(vacancy_payments.value);
+
+ var mgmt_payments = document.getElementById("mgmt_payments");
+ switch( mgmt_radios_val ){
+   case 'full':
+      mgmt_payments.value = (gross_rent * 0.10).toFixed(2); // 10%
+      break;
+   case 'partial':
+      mgmt_payments.value = (gross_rent * 0.06).toFixed(2);
+      break;
+   case 'self':
+      mgmt_payments.value = 49.95;
+      break;
+ }
+ mgmt_payments = parseInt(mgmt_payments.value);
 
  // show NOI
  var noi_now = gross_rent - loan_payments - tax_payments - maint_payments - vacancy_payments - mgmt_payments - insurance_payments - water_payments - snow_payments;
